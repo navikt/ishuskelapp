9fab21c1915692480d57022a5cc28958ddf13e78
Eirik Dahlen <dahleneirik@gmail.com>
2023-10-13 15:34:02 +0200

    IS: Add metrics for huskelapp
    Bare et forslag til metrics vi kan telle og vise i grafana. Gir disse mening, eller er det andre ting vi heller vil se på? Normalt sett teller vi kanskje i consumers/producers og ikke i servicen.

--- src/main/kotlin/no/nav/syfo/application/metric/Metric.kt
+++ src/main/kotlin/no/nav/syfo/application/metric/Metric.kt
@@ -1,8 +1,30 @@
 package no.nav.syfo.application.metric
 
+import io.micrometer.core.instrument.Counter
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
 const val METRICS_NS = "ishuskelapp"
 
 val METRICS_REGISTRY = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+const val HUSKELAPP_CREATED =
+    "${METRICS_NS}_huskelapp_created_count"
+val COUNT_HUSKELAPP_CREATED: Counter =
+    Counter.builder(HUSKELAPP_CREATED)
+        .description("Counts the number of huskelapp created")
+        .register(METRICS_REGISTRY)
+
+const val HUSKELAPP_VERSJON_CREATED =
+    "${METRICS_NS}_huskelapp_versjon_created_count"
+val COUNT_HUSKELAPP_VERSJON_CREATED: Counter =
+    Counter.builder(HUSKELAPP_VERSJON_CREATED)
+        .description("Counts the number of huskelapp versjon created")
+        .register(METRICS_REGISTRY)
+
+const val HUSKELAPP_REMOVED =
+    "${METRICS_NS}_huskelapp_removed_count"
+val COUNT_HUSKELAPP_REMOVED: Counter =
+    Counter.builder(HUSKELAPP_REMOVED)
+        .description("Counts the number of huskelapp removed")
+        .register(METRICS_REGISTRY)
